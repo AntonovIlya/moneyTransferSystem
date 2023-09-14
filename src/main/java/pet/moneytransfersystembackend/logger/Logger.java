30e1244ccb2bf11e4f82cbbe8c54b691f582ce87
antonov_ii <antonov_ii@rtsoft.ru>
2023-09-14 12:48:30 +0300

    bug fixes

--- src/main/java/pet/moneytransfersystembackend/logger/Logger.java
+++ src/main/java/pet/moneytransfersystembackend/logger/Logger.java
@@ -3,8 +3,6 @@ package pet.moneytransfersystembackend.logger;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class Logger {
 
